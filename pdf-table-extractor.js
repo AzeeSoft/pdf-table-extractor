05c287ebba288ef4fdc673b1479c451d46a5d846
Azee <aziztitu1996@gmail.com>
2019-01-06 13:35:55 -0600

    Turns pdf-table-extractor into a module and exposes the raw content.

--- pdf-table-extractor.js
+++ pdf-table-extractor.js
@@ -1,3 +1,200 @@
+var fs = require('fs');
+
+Image = function() {};
+
+// HACK few hacks to let PDF.js be loaded not as a module in global space.
+function xmlEncode(s) {
+    var i = 0,
+        ch;
+    s = String(s);
+    while (
+        i < s.length &&
+        (ch = s[i]) !== '&' &&
+        ch !== '<' &&
+        ch !== '"' &&
+        ch !== '\n' &&
+        ch !== '\r' &&
+        ch !== '\t'
+    ) {
+        i++;
+    }
+    if (i >= s.length) {
+        return s;
+    }
+    var buf = s.substring(0, i);
+    while (i < s.length) {
+        ch = s[i++];
+        switch (ch) {
+            case '&':
+                buf += '&amp;';
+                break;
+            case '<':
+                buf += '&lt;';
+                break;
+            case '"':
+                buf += '&quot;';
+                break;
+            case '\n':
+                buf += '&#xA;';
+                break;
+            case '\r':
+                buf += '&#xD;';
+                break;
+            case '\t':
+                buf += '&#x9;';
+                break;
+            default:
+                buf += ch;
+                break;
+        }
+    }
+    return buf;
+}
+
+global.btoa = function btoa(chars) {
+    var digits = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    var buffer = '';
+    var i, n;
+    for (i = 0, n = chars.length; i < n; i += 3) {
+        var b1 = chars.charCodeAt(i) & 0xff;
+        var b2 = chars.charCodeAt(i + 1) & 0xff;
+        var b3 = chars.charCodeAt(i + 2) & 0xff;
+        var d1 = b1 >> 2,
+            d2 = ((b1 & 3) << 4) | (b2 >> 4);
+        var d3 = i + 1 < n ? ((b2 & 0xf) << 2) | (b3 >> 6) : 64;
+        var d4 = i + 2 < n ? b3 & 0x3f : 64;
+        buffer += digits.charAt(d1) + digits.charAt(d2) + digits.charAt(d3) + digits.charAt(d4);
+    }
+    return buffer;
+};
+
+function DOMElement(name) {
+    this.nodeName = name;
+    this.childNodes = [];
+    this.attributes = {};
+    this.textContent = '';
+
+    if (name === 'style') {
+        this.sheet = {
+            cssRules: [],
+            insertRule: function(rule) {
+                this.cssRules.push(rule);
+            },
+        };
+    }
+}
+
+DOMElement.prototype = {
+    setAttributeNS: function DOMElement_setAttributeNS(NS, name, value) {
+        value = value || '';
+        value = xmlEncode(value);
+        this.attributes[name] = value;
+    },
+
+    appendChild: function DOMElement_appendChild(element) {
+        var childNodes = this.childNodes;
+        if (childNodes.indexOf(element) === -1) {
+            childNodes.push(element);
+        }
+    },
+
+    toString: function DOMElement_toString() {
+        var attrList = [];
+        for (i in this.attributes) {
+            attrList.push(i + '="' + xmlEncode(this.attributes[i]) + '"');
+        }
+
+        if (this.nodeName === 'svg:tspan' || this.nodeName === 'svg:style') {
+            var encText = xmlEncode(this.textContent);
+            return (
+                '<' +
+                this.nodeName +
+                ' ' +
+                attrList.join(' ') +
+                '>' +
+                encText +
+                '</' +
+                this.nodeName +
+                '>'
+            );
+        } else if (this.nodeName === 'svg:svg') {
+            var ns =
+                'xmlns:xlink="http://www.w3.org/1999/xlink" ' +
+                'xmlns:svg="http://www.w3.org/2000/svg"';
+            return (
+                '<' +
+                this.nodeName +
+                ' ' +
+                ns +
+                ' ' +
+                attrList.join(' ') +
+                '>' +
+                this.childNodes.join('') +
+                '</' +
+                this.nodeName +
+                '>'
+            );
+        } else {
+            return (
+                '<' +
+                this.nodeName +
+                ' ' +
+                attrList.join(' ') +
+                '>' +
+                this.childNodes.join('') +
+                '</' +
+                this.nodeName +
+                '>'
+            );
+        }
+    },
+
+    cloneNode: function DOMElement_cloneNode() {
+        var newNode = new DOMElement(this.nodeName);
+        newNode.childNodes = this.childNodes;
+        newNode.attributes = this.attributes;
+        newNode.textContent = this.textContent;
+        return newNode;
+    },
+};
+
+global.document = {
+    childNodes: [],
+
+    get currentScript() {
+        return { src: '' };
+    },
+
+    get documentElement() {
+        return this;
+    },
+
+    createElementNS: function(NS, element) {
+        var elObject = new DOMElement(element);
+        return elObject;
+    },
+
+    createElement: function(element) {
+        return this.createElementNS('', element);
+    },
+
+    getElementsByTagName: function(element) {
+        if (element === 'head') {
+            return [this.head || (this.head = new DOMElement('head'))];
+        }
+        return [];
+    },
+};
+
+// Run `gulp dist` to generate 'pdfjs-dist' npm package files.
+PDFJS = require('pdfjs-dist/build/pdf.js');
+var module_path = __dirname.substring(0, __dirname.lastIndexOf('/'));
+//PDFJS.workerSrc = module_path + '/pdfjs-dist/build/pdf.worker.js';
+//PDFJS.cMapUrl = module_path + '/pdfjs-dist/cmaps/';
+PDFJS.workerSrc = 'pdfjs-dist/build/pdf.worker.js';
+PDFJS.cMapUrl = 'pdfjs-dist/cmaps/';
+PDFJS.cMapPacked = true;
+
 // modify from https://github.com/mozilla/pdf.js/blob/master/examples/node/pdf2svg.js
 pdf_table_extractor_progress = function(result) {};
 
@@ -493,6 +690,10 @@ pdf_table_extractor = function(doc) {
                 })
                 .then(function() {
                     return page.getTextContent().then(function(content) {
+                        // console.log(JSON.stringify(content, null, 4));
+
+                        const contentBkp = JSON.parse(JSON.stringify(content));
+
                         tables = [];
                         table_pos = [];
                         for (var i = 0; i < horizons.length - 1; i++) {
@@ -550,6 +751,7 @@ pdf_table_extractor = function(doc) {
                                 merge_alias: merge_alias,
                                 width: verticles.length - 1,
                                 height: horizons.length - 1,
+                                content: contentBkp,
                             });
                         }
                         result.currentPages++;
@@ -568,3 +770,17 @@ pdf_table_extractor = function(doc) {
         return result;
     });
 };
+
+pdf_table_extractor_run = function(pdfPath, success, error) {
+    var data = new Uint8Array(fs.readFileSync(pdfPath));
+
+    // Will be using promises to load document, pages and misc data instead of
+    // callback.
+    PDFJS.getDocument(data)
+        .then(pdf_table_extractor)
+        .then(success, error);
+};
+
+if (typeof module !== 'undefined') {
+    module.exports = pdf_table_extractor_run;
+}
